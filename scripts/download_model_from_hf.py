0daa310668307ff7fbcb75de4022c6202b7a2348
Matt Rutkowski <mrutkows@us.ibm.com>
2024-12-12 10:18:05 -0600

    Improve CI convert/quantize to be parameterized
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/download_model_from_hf.py
+++ scripts/download_model_from_hf.py
@@ -1,29 +1,54 @@
 import sys
-from huggingface_hub import hf_hub_download, snapshot_download
+from huggingface_hub import hf_hub_download, snapshot_download, list_repo_files
+from typing import List
 
-# Specify the model repository ID and filename
-TEST_REPO_ORG = "ibm-granite"
-TEST_REPO_NAME = "granite-rag-3.0-8b-lora"
-models_dir = "models"
+# Constants (defaults)
+DEFAULT_MODEL_DIR = "models"
+DEFAULT_REPO_ORG = "ibm-granite"
 
-# Download the file
-def download_model(models_dir=models_dir, repo_id=""):
+
+# TODO: minimize downloaded files to only those needed for conversion
+def download_model_files(models_dir:str="", repo_id:str="", files:List[str]=[]) -> str:
     print(f">>> models_dir: {models_dir}, repo_id: {repo_id}")
+    local_dir = models_dir + "/" + repo_id
+    # TODO: default to some subset of all files in model repo.
+    #files = list_repo_files(repo_id=model_name, token=hf_token)    
+    for file_name in files:
+        download_dir = hf_hub_download(
+                repo_id=repo_id,
+                filename=file_name,
+                local_dir=local_dir,
+            )  
+    return download_dir
+    
+    
+def download_model_snapshot(models_dir:str="", repo_id:str="") -> str:
+    print(f">>> models_dir='{models_dir}', repo_id='{repo_id}'")
+    if models_dir == "":
+        print("models_dir is empty")
+        return    
     if repo_id == "":
         print("repo_id is empty")
         return
     local_dir = models_dir + "/" + repo_id
     print(f"local_dir: {local_dir}")
-    download_dir = snapshot_download(repo_id=repo_id, local_dir=local_dir)
-    print(f"download_dir: {download_dir}")
+    download_dir = snapshot_download(
+            repo_id=repo_id, 
+            local_dir=local_dir,
+        )  
+    return download_dir 
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python download_model_from_hf.py <repo_org> <repo_name>")
+    if len(sys.argv) != 4:
+        print("Usage: python download_model_from_hf.py <models_dir> <repo_org> <repo_name>")
     else:
-        repo_org = sys.argv[1]
-        repo_name = sys.argv[2]
+        fx_name = sys.argv[0]
+        models_dir = sys.argv[1]
+        repo_org = sys.argv[2]
+        repo_name = sys.argv[3]
         repo_id = repo_org + "/" + repo_name
-        print(f">>> repo_org: {repo_org}, repo_name: {repo_name}, repo_id: {repo_id}")
-        download_model(models_dir=models_dir, repo_id=repo_id)
+        print(f">> {fx_name}: models_dir='{models_dir}', repo_org='{repo_org}', repo_name='{repo_name}'")
+        # Note: this downloads everything... TODO: download only the necessary files.
+        download_dir = download_model_snapshot(models_dir=models_dir, repo_id=repo_id)
+        print(f"download_dir: {download_dir}") 
