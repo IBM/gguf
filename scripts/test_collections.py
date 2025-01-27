1546d5fc9e8d4dcef0dabf5d0a6b7457d60d5978
Matt Rutkowski <mrutkows@us.ibm.com>
2025-01-27 10:45:45 -0600

    Clean up collection functions
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/test_collections.py
+++ scripts/test_collections.py
@@ -1,21 +1,40 @@
 import os
-import sys
-from huggingface_hub import hf_hub_download, snapshot_download, list_repo_files
-from typing import List
 import requests
 
-from huggingface_hub import list_collections, create_collection, Collection, CollectionItem
+from huggingface_hub import list_collections, create_collection, add_collection_item, Collection, CollectionItem
+from huggingface_hub import list_repo_files
 from huggingface_hub.utils import HfHubHTTPError
 
 # retrieve secrets
 hf_token=os.environ['HF_TOKEN']
 
-def get_collection_by_title(hf_owner:str="", hf_token:str=hf_token, title:str="") -> Collection:
+###########################################
+# Repos
+###########################################
+
+###########################################
+# Collections
+###########################################
+
+def get_collections_in_namespace(hf_owner:str="", hf_token:str=hf_token) -> None:
+    collections = list_collections(owner=hf_owner, token=hf_token)
+    return collections
+
+
+def get_collection_by_title(hf_owner:str="", title:str="", hf_token:str=hf_token) -> Collection:
+    if hf_owner == "":
+        print("Please provide an owner (username or organization) for the collection")
+        return False
+    if title == "":
+        print("Please provide a title for the new collection")
+        return False      
+    
     collections = get_collections_in_namespace(hf_owner=hf_owner, hf_token=hf_token)
     for c in collections:
         if c.title == title:
             return c
     return None
+
 
 def list_collection_attributes(collections:Collection=None, list_items:bool=False) -> None:
     if collections is None:
@@ -24,9 +43,9 @@ def list_collection_attributes(collections:Collection=None, list_items:bool=Fals
     for collection in collections:
         print(f"---")
         print(f"title: `{collection.title}`, private: {collection.private}, description: `{collection.description}`, slug: `{collection.slug}`")
-        print(f"---")
         if list_items:
             list_collection_items(collection=collection) 
+
 
 def list_collection_items(collection:Collection=None) -> None:
     if collection is None:
@@ -34,36 +53,74 @@ def list_collection_items(collection:Collection=None) -> None:
         return
     num_items = len(collection.items)
     if num_items > 0:
-        print(f"items ({num_items}): {collection.items}")
+        for item in collection.items:
+            print(f"item_id: '{item.item_id}' ({item.item_type}), position: '{item.position}', item_object_id: '{item.item_object_id}'")        
+            if item.note is not None:
+                print(f"\t| {item.note}")
     else:
-        print(f"None")
+        print(f"(no items)")
 
-def get_collections_in_namespace(hf_owner:str="", hf_token:str=hf_token) -> None:
-    collections = list_collections(owner=hf_owner, token=hf_token)
-    return collections
 
-def create_collection_in_namespace(hf_owner:str="", title:str="", description:str="", private:bool=True, hf_token:str=hf_token) -> Collection:
+def safe_create_collection_in_namespace(hf_owner:str="", title:str="", description:str="", private:bool=True, hf_token:str=hf_token) -> Collection:
+    if hf_owner == "":
+        print("Please provide an owner (username or organization) for the collection")
+        return False
     if title == "":
-        print("Please provide a title for the new collection")
+        print("Please provide a title for the collection")
         return False
     if description == "":
-        print("Please provide a description for the new collection")
+        print("Please provide a description for the collection")
         return False        
     
     try:
-        collection = create_collection(
-            title=title,
-            description=description,
-            private=private,
+        # We want to test if the collection already exists before creating it (and not rely on exceptions)
+        collection = get_collection_by_title(hf_owner=hf_owner, title=title, hf_token=hf_token)
+        if collection is None:
+            collection = create_collection(
+                title=title,
+                description=description,
+                private=private,
+                token=hf_token,
+        )
+        else:
+            print(f"Collection '{title}' already exists in namespace '{hf_owner}'")
+    except HfHubHTTPError as exc:
+        print(f"HfHubHTTPError: {exc.server_message}, collection.title: '{title}'")
+    except requests.exceptions.HTTPError as exc:
+        print(f"HTTPError: {exc}")
+    except requests.exceptions.ConnectionError as exc:
+        print(f"ConnectionError: {exc}")
+    except requests.exceptions.Timeout as exc:
+        print(f"Timeout: {exc}")
+    except requests.exceptions.RequestException as exc:
+        print(f"RequestException: {exc}")
+    else: 
+        return collection
+    return None
+
+
+def add_update_collection_model(collection_slug:str="", repo_name:str="", note:str="", hf_token:str=hf_token) -> Collection:
+    if collection_slug == "":
+        print("Please provide a slug (ID) for the collection.")
+        return False
+    if repo_name == "":
+        print("Please provide a repo_name for the model repo.")
+        return False   
+    # If an item already exists in a collection (same item_id/item_type pair), 
+    # an HTTP 409 error will be raised. 
+    # You can choose to ignore this error by setting exists_ok=True    
+    try:
+        collection = add_collection_item(
+            collection_slug,
+            item_id=repo_name,
+            item_type="model",
+            note=note,
+            exists_ok=True,
             token=hf_token,
         )
     except HfHubHTTPError as exc:
-        print(f"HfHubHTTPError: {exc.server_message}")
+        print(f"HfHubHTTPError: {exc.server_message}, collection.title: '{title}'")
     except requests.exceptions.HTTPError as exc:
-        # has_attribute = hasattr(exc, "server_message")
-        #if has_attribute:
-        #     print(f"HfHubHTTPError: {exc.server_message}")
-        # else:
         print(f"HTTPError: {exc}")
     except requests.exceptions.ConnectionError as exc:
         print(f"ConnectionError: {exc}")
@@ -79,31 +136,37 @@ if __name__ == "__main__":
     # Test private repo.
     collections = get_collections_in_namespace(hf_owner="mrutkows", hf_token=hf_token)  
     list_collection_attributes(collections=collections, list_items=True)  
-    
-    # Test ibm-granite
-    collections = get_collections_in_namespace(hf_owner="ibm-granite", hf_token=hf_token) 
-    list_collection_attributes(collections=collections, list_items=True)
    
     # Test private repo.   
     existing_collection = get_collection_by_title(hf_owner="mrutkows", title="Granite 3.2.0", hf_token=hf_token)
     if existing_collection is not None:
         list_collection_items(existing_collection) 
-        
+     
+    # Test ibm-granite
+    collections = get_collections_in_namespace(hf_owner="ibm-granite", hf_token=hf_token) 
+    list_collection_attributes(collections=collections, list_items=True)
+            
     # Test ibm-granite 
     existing_collection = get_collection_by_title(hf_owner="ibm-granite", title="Granite 3.1 Language Models", hf_token=hf_token)
     if existing_collection is not None:
         list_collection_items(existing_collection)     
 
     # Test private repo.         
-    collection = create_collection_in_namespace(hf_owner="mrutkows", title="Granite 3.2.0", description="IBM Granite 3.2.0", hf_token=hf_token)            
-    
     from huggingface_hub import create_repo
-    try:
-        create_repo("mrutkows/test-q2-k-gguf")
-    except HfHubHTTPError as exc:
-        print(f"HfHubHTTPError: {exc.server_message}")        
-    except Exception as exc:
-        print(f"Exception: {exc}")
+    collection_title = "Granite 3.2.0"
+    repo_name = "mrutkows/test-q2-k-gguf"    
+    repo_owner = "mrutkows" 
+    # c = get_collection_by_title(hf_owner=repo_owner, title=collection_title, hf_token=hf_token)
+    # if c is None:
+    collection = safe_create_collection_in_namespace(hf_owner=repo_owner, title=collection_title, description="IBM Granite 3.2.0", hf_token=hf_token)            
+    if collection is not None:
+        list_collection_items(collection)  
+        add_update_collection_model(
+            collection_slug=collection.slug, 
+            repo_name=repo_name, 
+            note="test note",
+            hf_token=hf_token)
+        list_collection_items(collection)  
     
 # from huggingface_hub import HfApi
 # api = HfApi()
