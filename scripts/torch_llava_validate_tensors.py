ffc70bc8f4a8b2f211cfb5a7f4c9ced105b408a1
Matt Rutkowski <mrutkows@us.ibm.com>
2025-02-25 14:05:35 -0600

    Support llava model build and test
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

---
+++ scripts/torch_llava_validate_tensors.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import torch
+
+def validate_llava_tensors(file_llava_clip:str, file_llava_projector:str) -> None:
+    if not file_llava_clip:
+        raise ValueError(f"invalid: file_llava_clip: {file_llava_clip}")
+    if not file_llava_projector:
+        raise ValueError(f"invalid: file_llava_projector: {file_llava_projector}")    
+
+    encoder_tensors = torch.load(file_llava_clip)
+    projector_tensors = torch.load(file_llava_projector)
+
+    assert len(encoder_tensors) > 0
+    assert len(projector_tensors) > 0
+
+    keys_projector = projector_tensors.keys()
+    print("encoder keys: \n", encoder_tensors.keys())
+    print("projector keys: \n", keys_projector)
+
+    with open("projector_keys.txt", "w") as file:
+        file.write(keys_projector)
+    
+if __name__ == "__main__":   
+    arg_len = len(sys.argv)
+    if arg_len < 3:   
+        script_name = os.path.basename(__file__)
+        print(f"Usage: python {script_name} <file_llava_clip> <file_llava_projector>")
+        print(f"Actual: sys.argv[]: '{sys.argv}'")
+        sys.exit(1)
+       
+    # Parse input arguments into named params.   
+    fx_name = sys.argv[0]   
+    file_llava_clip = sys.argv[1]
+    file_llava_projector = sys.argv[2]   
+    
+    # Print input variables being used for this run
+    print(f">> {fx_name}: llava_clip'{file_llava_clip}', file_llava_projector='{file_llava_projector}'")     
+    
+    # invoke fx
+    validate_llava_tensors(file_llava_clip=file_llava_clip, file_llava_projector=file_llava_projector)  
+    
