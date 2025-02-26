b8d72757668a5de0b364a9187b303a6e5bf2eb2d
Matt Rutkowski <mrutkows@us.ibm.com>
2025-02-26 08:09:26 -0600

    Support llava model build and test
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/torch_llava_save_llm.py
+++ scripts/torch_llava_save_llm.py
@@ -1,26 +1,41 @@
 import os
+import sys
 import transformers
 
-MODEL_PATH = os.getenv("LOCAL_MODEL_PATH")    
-print(f"MODEL_PATH={MODEL_PATH}")
+def save_llm_from_llava(source_repo:str, target_repo:str):
+    if not source_repo:
+        raise ValueError("source_repo is unset!")
+    if not target_repo:   
+        raise ValueError("target_repo is unset!")
 
-if not MODEL_PATH:
-    raise ValueError("LOCAL_MODEL_PATH is unset!")
+    tokenizer = transformers.AutoTokenizer.from_pretrained(source_repo)
 
-ENCODER_PATH = os.getenv("LLM_EXPORT_PATH")
-print(f"ENCODER_PATH={MODEL_PATH}") 
+    # NOTE: granite vision support was added to transformers very recently (4.49);
+    # if you get size mismatches, your version is too old.
+    # If you are running with an older version, set `ignore_mismatched_sizes=True`
+    # as shown below; it won't be loaded correctly, but the LLM part of the model that
+    # we are exporting will be loaded correctly.
+    model = transformers.AutoModelForImageTextToText.from_pretrained(source_repo, ignore_mismatched_sizes=True)
 
-if not ENCODER_PATH:   
-    raise ValueError("ENCODER_PATH is unset!")
+    tokenizer.save_pretrained(target_repo)
+    model.language_model.save_pretrained(target_repo)
 
-tokenizer = transformers.AutoTokenizer.from_pretrained(MODEL_PATH)
-
-# NOTE: granite vision support was added to transformers very recently (4.49);
-# if you get size mismatches, your version is too old.
-# If you are running with an older version, set `ignore_mismatched_sizes=True`
-# as shown below; it won't be loaded correctly, but the LLM part of the model that
-# we are exporting will be loaded correctly.
-model = transformers.AutoModelForImageTextToText.from_pretrained(MODEL_PATH, ignore_mismatched_sizes=True)
-
-tokenizer.save_pretrained(ENCODER_PATH)
-model.language_model.save_pretrained(ENCODER_PATH)
+if __name__ == "__main__":   
+    arg_len = len(sys.argv)
+    if arg_len < 3:   
+        script_name = os.path.basename(__file__)
+        print(f"Usage: python {script_name} <source_repo> <target_repo>")
+        print(f"Actual: sys.argv[]: '{sys.argv}'")
+        sys.exit(1)
+       
+    # Parse input arguments into named params.   
+    fx_name = sys.argv[0]   
+    source_repo = sys.argv[1]
+    target_repo = sys.argv[2]   
+    
+    # Print input variables being used for this run
+    print(f">> {fx_name}: source_repo='{source_repo}' target_repo='{target_repo}'")     
+    
+    # invoke fx
+    save_llm_from_llava(source_repo=source_repo, target_repo=target_repo)  
+   
