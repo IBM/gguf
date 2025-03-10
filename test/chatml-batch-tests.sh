1569e5b0aa98165e9e338cca58f8a2bea38a2e21
Matt Rutkowski <mrutkows@us.ibm.com>
2025-03-10 16:23:18 -0500

    create scripts to test chatml templates for guardian model
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- test/chatml-batch-tests.sh
+++ test/chatml-batch-tests.sh
@@ -7,20 +7,20 @@ messages_path="messages/granite/guardian/"
 output_dir=temp/
 
 message_test_files=( \
-    "user-harm.json" \
-    "user-asst-harm.json" \
-    "user-asst-evasiveness.json" \
-    "user-harm-social-bias.json" \
-    "rag-context-relevance.json" \
-    "rag-function-calling.json" \
-    "config-custom-risk-name-and-defn.json" \
-    "config-existing-risk-name-and-no-defn.json" \
-    )
+  "1-1-user-harm.json" \
+  "1-1-user-asst-harm.json" \
+  "1-2-user-harm-social-bias.json" \
+  "1-8-user-asst-evasiveness.json" \
+  "3-1-rag-context-relevance.json" \
+  "4-1-rag-function-calling.json" \
+  "5-1-config-custom-risk-name-and-defn.json" \
+  "5-2-config-existing-risk-name-and-no-defn.json" \
+)
 
 message_test_files_with_errors=( \
-    "config-custom-risk-no-defn.json" \
-    "config-existing-risk-name-and-new-defn.json" \
-    )
+  "5-err-1-config-custom-risk-no-defn.json" \
+  "5-err-2-config-existing-risk-name-and-new-defn.json" \
+)
 
 mkdir $output_dir
 
