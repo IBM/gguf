913400d50cfc4dc7be5464c1292c0d92273528a5
Matt Rutkowski <mrutkows@us.ibm.com>
2025-03-11 11:47:07 -0500

    create scripts to test chatml templates for guardian model
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- test/chatml-batch-tests.sh
+++ test/chatml-batch-tests.sh
@@ -6,14 +6,20 @@ chat_ml_template="templates/llama.cpp/granite-guardian-chatml.test.txt"
 messages_path="messages/granite/guardian/"
 output_dir=temp/
 
+# Note: there must be no trailing whitespace after each line continuation char('\')
+# AND only 1 space char. before.
 message_test_files=( \
-  "1-1-user-harm.json" \
-  "1-1-user-asst-harm.json" \
-  "1-1-user-asst-harm-no-system.json" \
-  "1-1-config-user-harm-no-system.json" \
-  "1-2-user-harm-social-bias.json" \
-  "1-7-user-harm-engagement.json" \
-  "1-8-user-asst-evasiveness.json" \
+  "1-1-harm-user-asst-harm-no-system.json" \
+  "1-1-harm-user-asst-harm.json" \
+  "1-1-harm-user-harm-config-no-system.json" \
+  "1-1-harm-user-harm.json" \
+  "1-2-harm-user-social-bias.json" \
+  "1-3-harm-user-jailbreak.json" \
+  "1-4-harm-user-profanity.json" \
+  "1-5-harm-user-unethical-behavior.json" \
+  "1-6-harm-user-violence.json" \
+  "1-7-harm-user-engagement.json" \
+  "1-8-harm-user-asst-evasiveness.json" \
   "3-1-rag-context-relevance.json" \
   "4-1-rag-function-calling.json" \
   "5-1-config-custom-risk-name-and-defn.json" \
@@ -21,9 +27,10 @@ message_test_files=( \
 )
 
 message_test_files_with_errors=( \
-  "1-err-1-user-harm-invalid-primary.json" \
-  "1-err-2-user-harm-no-primary.json" \
-  "1-err-3-user-harm-invalid-secondary.json" \
+  "1-err-2-harm-invalid-primary.json" \
+  "1-err-4-harm-invalid-system-risk-name.json" \
+  "1-err-1-harm-no-primary-role.json" \
+  "1-err-3-harm-invalid-secondary-role.json" \
   "5-err-1-config-custom-risk-no-defn.json" \
   "5-err-2-config-existing-risk-name-and-new-defn.json" \
 )
