246d3946416f9b76dbfcfc2b4848bc6c8a3c8507
Matt Rutkowski <mrutkows@us.ibm.com>
2025-03-26 10:55:58 -0500

    improve hf scripts
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/hf_repos_create.py
+++ scripts/hf_repos_create.py
@@ -41,42 +41,18 @@ def safe_create_repo_in_namespace(repo_id:str="", private:bool=True, hf_token:st
 
 
 if __name__ == "__main__":
-    # arg_len = len(sys.argv)
-    # if arg_len < 5:
-    #     script_name = os.path.basename(__file__)
-    #     print(f"Usage: python {script_name} <target_owner:str> <collection_config:str> <family:str> <private:bool> <hf_token:str>")
-    #     print(f"Actual: sys.argv[]: '{sys.argv}'")
-    #     # Exit with an error code
-    #     sys.exit(1)
-
-    # # Parse input arguments into named params.
-    # fx_name = sys.argv[0]
-    # target_owner = sys.argv[1]
-    # # TODO: "private should default to True (confirmed by "pre" tags);
-    # # if workflow was started with a "release" tag, then change to False
-    # collection_config = sys.argv[2]
-    # family = sys.argv[3]
-    # private = sys.argv[4]
-    # hf_token = sys.argv[5]
-
     try:
-        # fx_name = sys.argv[0]
-        # private = sys.argv[4]
-        # # private needs to be a boolean
-        # if type(private) is str:
-        #     print(f"[WARNING] private='{private}' is a string. Converting to boolean...")
-        #     if private.lower() == "true":
-        #         private = True
-        #     else:
-        #         private = False
-
         parser = argparse.ArgumentParser(description=__doc__, exit_on_error=False)
         parser.add_argument("target_owner", help="Target HF organization owner for repo. create")
         parser.add_argument("collection_config", help="The input text to search within")
         parser.add_argument('family', help='Granite family (i.e., instruct|vision|guardian)')
         parser.add_argument('private', default="True", help='Create the repo. as private')
         parser.add_argument('hf_token', help='HF access token')
+        parser.add_argument('--verbose', default=True, action='store_true', help='Enable verbose output')
+        parser.add_argument('--debug', default=False, action='store_false', help='Enable debug output')
 
+
+        # parse argv[] values
         args = parser.parse_args()
 
         # Print input variables being used for this run
@@ -109,14 +85,16 @@ if __name__ == "__main__":
 
             # upload all models associated with the collection
             for item_defn in collection_items:
-                print(f"item_defn: '{item_defn}'")
+                if( args.debug):
+                    print(f"item_defn: '{item_defn}'")
                 item_type = item_defn["type"]
                 repo_id = item_defn["repo_id"]
                 item_family = item_defn["family"]
                 if args.family == item_family:
                     repo_org, repo_name = os.path.split(repo_id)
-                    print(f"[INFO] Creating repo: repo_id: '{repo_id}'...")
-                    print(f"[INFO] Creating repo: repo_org: '{repo_org}', repo_name: '{repo_name}'...")
+                    if args.verbose:
+                        print(f"[INFO] Creating repo: repo_id: '{repo_id}'...")
+                        print(f"[INFO] Creating repo: repo_org: '{repo_org}', repo_name: '{repo_name}'...")
 
                     repoUrl = safe_create_repo_in_namespace(
                         repo_id=repo_id,
@@ -128,8 +106,8 @@ if __name__ == "__main__":
                         # Something went wrong creating
                         print(f"[ERROR] Repo: repo_id: '{repo_id}' not created.")
                         sys.exit(1)
-
-                    print(f"repoUrl: '{repoUrl}')")
+                    if args.verbose:
+                        print(f"[SUCCESS] Repo. created. repoUrl: '{repoUrl}')")
 
     except SystemExit as se:
         print(f"Usage: {parser.format_usage()}")
