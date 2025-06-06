43d34a59576e8d4d5231769de4f9a33cae89b16f
Matt Rutkowski <mrutkows@us.ibm.com>
2025-06-06 14:31:31 -0500

    Add ollama jobs
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/array_append_values.py
+++ scripts/array_append_values.py
@@ -9,6 +9,8 @@ if __name__ == "__main__":
     try:
         input_array = json.loads(sys.argv[1])
         values_to_append = json.loads(sys.argv[2])
+        print(f"input_array: {input_array}")
+        print(f"values_to_append: {values_to_append}")
         result = append_to_array(input_array, values_to_append)
         print(json.dumps(result))
     except (IndexError, json.JSONDecodeError) as e:
