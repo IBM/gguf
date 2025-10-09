ad5959a5274b66d11f396fd3dfa717a880e9080c
Matt Rutkowski <mrutkows@us.ibm.com>
2025-10-09 14:42:43 -0500

    Add Ollama model name generation tests
    Signed-off-by: Matt Rutkowski <mrutkows@us.ibm.com>

--- scripts/test/test_get_partner_model_name.sh
+++ scripts/test/test_get_partner_model_name.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 set -e # stop execution on error
 
-GREEN='\033[0;32m'
 RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+LIGHT_GRAY='\033[0;90m'
+
+BRIGHT_WHITE='\033[0;97m'
+DEFAULT='\033[0;39m'
 RESET='\033[0m'
 
+# Run matrix
+RUN_G4_TESTS=0
+RUN_G3_3_TESTS=1
+RUN_G3_2_TESTS=1
+
 # Activate the desired Conda environment
+readonly PARTNER="ollama"
 readonly CONDA_RUN="conda run -n llama.cpp"
-readonly PYTHON_SCRIPT="./scripts/get_partner_model_name.py -p ollama -m "
-readonly PYTHON_SCRIPT_DEBUG="./scripts/get_partner_model_name.py --debug -p ollama -m "
+readonly PYTHON_SCRIPT="./scripts/get_partner_model_name.py -p ${PARTNER} -m "
+readonly PYTHON_SCRIPT_DEBUG="./scripts/get_partner_model_name.py --debug -p ${PARTNER} -m "
 
 # Function to print a success message and exit
 success() {
-  echo -e "${GREEN}[SUCCESS] $1${RESET}"
+  echo -e "${GREEN}[SUCCESS] ${LIGHT_GRAY}hf: ${WHITE}$1 ${LIGHT_GRAY}=> ${LIGHT_GRAY}${PARTNER}: ${CYAN}$2 ${RESET}"
   return 0
 }
 
 # Function to print an error message and exit with a non-zero status
 error() {
-  echo -e "${RED}[ERROR] $1${RESET}" # >&2
+  echo -e "${RED}[ERROR] ${LIGHT_GRAY}hf: ${WHITE}$1 ${LIGHT_GRAY}=> ${LIGHT_GRAY}${PARTNER}: ${CYAN}$2 ${LIGHT_GRAY}(expected:${YELLOW}$3 ${RESET}" # >&2
   return 0
 }
 
 ##############
 # G4
 ##############
+
+if [[ $RUN_G4_TESTS -eq 1 ]]; then
+
+echo -e "${YELLOW}Running Granite 4 tests..."
 
 # TODO: nano-1b, nano-1b-base
 
@@ -33,11 +52,11 @@ error() {
 # expected="granite4:1b-nano-h-q4_K_M"
 # output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 # if ! [[ "$output" == $expected ]]; then
-#     error "$input => $output, expected $expected"
+#     error "$input" "$output" "$expected"
 #     # Re-run script with --debug (do not need to activate conda env. again)
 #     python $PYTHON_SCRIPT_DEBUG $input
 # else
-#     success "$input => $output"
+#     success "$input" "$output"
 # fi
 
 # h-nano-1b-base
@@ -45,11 +64,11 @@ error() {
 # expected="granite4:1b-nano-h-base-q4_K_M"
 # output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 # if ! [[ "$output" == $expected ]]; then
-#     error "$input => $output, expected $expected"
+#     error "$input" "$output" "$expected"
 #     # Re-run script with --debug (do not need to activate conda env. again)
 #     python $PYTHON_SCRIPT_DEBUG $input
 # else
-#     success "$input => $output"
+#     success "$input" "$output"
 # fi
 
 # TODO: nano-300m, nano-300m-base
@@ -61,9 +80,9 @@ input="granite-4.0-micro-Q8_0.gguf"
 expected="granite4:micro-q8_0"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # micro-base
@@ -71,9 +90,9 @@ input="granite-4.0-micro-base-Q4_1.gguf"
 expected="granite4:micro-base-q4_1"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-micro
@@ -81,9 +100,9 @@ input="granite-4.0-h-micro-Q2_K.gguf"
 expected="granite4:micro-h-q2_K"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-micro-base
@@ -91,9 +110,9 @@ input="granite-4.0-h-micro-base-Q5_K_S.gguf"
 expected="granite4:micro-h-base-q5_K_S"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-tiny
@@ -101,9 +120,9 @@ input="granite-4.0-h-tiny-Q5_1.gguf"
 expected="granite4:tiny-h-q5_1"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-tiny-base
@@ -111,9 +130,9 @@ input="granite-4.0-h-tiny-base-Q5_K_M.gguf"
 expected="granite4:tiny-h-base-q5_K_M"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-small
@@ -121,9 +140,9 @@ input="granite-4.0-h-small-Q4_K_M.gguf"
 expected="granite4:small-h-q4_K_M"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
 fi
 
 # h-small-base
@@ -131,9 +150,59 @@ input="granite-4.0-h-small-base-Q4_K_M.gguf"
 expected="granite4:small-h-base-q4_K_M"
 output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 if ! [[ "$output" == "$expected" ]]; then
-    error "$input => $output, expected $expected"
+    error "$input" "$output" "$expected"
 else
-    success "$input => $output"
+    success "$input" "$output"
+fi
+
+fi
+
+##############
+# G3.3
+##############
+
+if [[ $RUN_G3_3_TESTS -eq 1 ]]; then
+
+echo -e "${YELLOW}Running Granite 3.3 tests..."
+
+input="granite-vision-3.3-2b-embedding-f16.gguf"
+expected="granite3.3-vision:2b-f16"
+output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
+if ! [[ "$output" == "$expected" ]]; then
+    error "$input" "$output" "$expected"
+else
+    success "$input" "$output"
+fi
+
+fi
+
+##############
+# G3.2
+##############
+
+if [[ $RUN_G3_2_TESTS -eq 1 ]]; then
+
+# Embedding models
+echo -e "${YELLOW}Running Granite 3.2 tests..."
+
+input="granite-embedding-30m-english-q8_0.gguf"
+expected="granite-embedding:30m-english-q8_0"
+output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
+if ! [[ "$output" == "$expected" ]]; then
+    error "$input" "$output" "$expected"
+else
+    success "$input" "$output"
+fi
+
+input="granite-embedding-278m-multilingual-q8_0.gguf"
+expected="granite-embedding:278m-multilingual-q8_0"
+output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
+if ! [[ "$output" == "$expected" ]]; then
+    error "$input" "$output" "$expected"
+else
+    success "$input" "$output"
+fi
+
 fi
 
 ##############
@@ -144,25 +213,25 @@ fi
 # expected="granite3-moe:1b-base-q4_1"
 # output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 # if ! [[ "$output" == $expected ]]; then
-#     error "$input => $output, expected $expected"
+#     error "$input" "$output" "$expected"
 # else
-#     success "$input => $output"
+#     success "$input" "$output"
 # fi
 
 # input="granite-3.0-1b-a400m-base-Q2_K.gguf"
 # expected="granite3-moe:1b-base-q2_K"
 # output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 # if ! [[ "$output" == $expected ]]; then
-#     error "$input => $output, expected $expected"
+#     error "$input" "$output" "$expected"
 # else
-#     success "$input => $output"
+#     success "$input" "$output"
 # fi
 
 # input="granite-guardian-3.0-2b-Q4_K_M.gguf"
 # expected="granite3-guardian:2b-q4_K_M"
 # output=$($CONDA_RUN python $PYTHON_SCRIPT $input)
 # if ! [[ "$output" == $expected ]]; then
-#     error "$input => $output, expected $expected"
+#     error "$input" "$output" "$expected"
 # else
-#     success "$input => $output"
+#     success "$input" "$output"
 # fi
